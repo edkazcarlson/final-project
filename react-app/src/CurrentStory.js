965504040fbb3ba7f483973c93144103fb7a93b7
MargaretEarnest <molly7312000@gmail.com>
2020-10-16 19:56:53 -0400

    Cleaning and reformatting again

--- react-app/src/CurrentStory.js
+++ react-app/src/CurrentStory.js
@@ -143,12 +143,15 @@ export default class CurrentStory extends React.Component {
                                        label="Next input" type="text" placeholder="enter a word or phrase"
                                        variant="filled" margin="normal" InputLabelProps={{shrink: true}}
                             />
-                        <input style = {{visibility: 'hidden', height: '0px', width: '0px'}}type="submit" onClick={this.addWord.bind(this)}/>
-                    </form>
-                    <br/>
-                    <h2 className="content">{(yarray !== undefined) ? ((skipTry < 0) ? 0 : skipTry) : ""} more users must contribute before you can.</h2>
-                    <h2 className="lowPriority">THERE ARE {this.state.maxWords - this.state.curWordCount} {this.state.isWordType ? 'WORDS' : 'PHRASES'} REMAINING.</h2>
-                </div>
+                            <input style={{visibility: 'hidden', height: '0px', width: '0px'}} type="submit"
+                                   onClick={this.addWord.bind(this)}/>
+                        </form>
+                        <br/>
+                        <h2 className="content">{(yarray !== undefined) ? ((skipTry < 0) ? 0 : skipTry) : ""} more users
+                            must contribute before you can.</h2>
+                        <h2 className="lowPriority">THERE
+                            ARE {this.state.maxWords - this.state.curWordCount} {this.state.isWordType ? 'WORDS' : 'PHRASES'} REMAINING.</h2>
+                    </div>
                 </ThemeProvider>
             </div>
         );
