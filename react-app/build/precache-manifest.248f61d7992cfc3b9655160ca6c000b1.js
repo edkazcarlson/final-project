39bf55de3ae846aa8dd2d90512e4bb7a76fd81c1
edkazcarlson <ekcarlson@wpi.edu>
2020-10-16 18:19:39 -0700

    final deploy

---
+++ react-app/build/precache-manifest.248f61d7992cfc3b9655160ca6c000b1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a245d782a946e5fc7b981f3796db3f8e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "273890bc6f8fb7cbe62b",
+    "url": "/static/css/main.c821aaf5.chunk.css"
+  },
+  {
+    "revision": "df8757cf805b0e19c429",
+    "url": "/static/js/2.c18f3d1b.chunk.js"
+  },
+  {
+    "revision": "1ad004c81198dcbe8a01addf0c7a8085",
+    "url": "/static/js/2.c18f3d1b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "273890bc6f8fb7cbe62b",
+    "url": "/static/js/main.7c8b8980.chunk.js"
+  },
+  {
+    "revision": "5387050906b8ed53cb42",
+    "url": "/static/js/runtime-main.ec2d9410.js"
+  }
+]);
